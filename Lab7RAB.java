c33030ed998572a1ea392e0b2e119f6b41401cf7
Bilal Nasir <106511378+Bilal-110@users.noreply.github.com>
2022-07-15 11:49:14 +0500

    Add files via upload

---
+++ Lab7RAB.java
@@ -0,0 +1,17 @@
+class Addition
+{
+	int x,y;
+ Addition(int a, int b)
+{
+x=a;
+y=b;
+}
+}
+public class Lab7RAB
+{
+public static void main(String args[])
+{
+Addition P =new Addition(10,20);
+System.out.println(P.x+P.y);
+}
+}
---
+++ LabRAB.java
@@ -0,0 +1,17 @@
+class Addition
+{
+	int x,y;
+ Addition()
+{
+x=10;
+y=20;
+}
+}
+public class LabRAB
+{
+public static void main(String args[])
+{
+Addition P =new Addition();
+System.out.println(P.x+P.y);
+}
+}
