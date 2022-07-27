f49e4198aac02aad987e6021616517b976d97676
Bilal Nasir <106511378+Bilal-110@users.noreply.github.com>
2022-07-27 12:48:57 +0500

    Add files via upload

---
+++ TestArray2D.java
@@ -0,0 +1,17 @@
+public class TestArray2D
+{
+	public static void main(String[]ags)
+	{
+		 int x[][] = new int[2][2];
+		 x[0][0] = 50;
+		 x[0][1] = 60;
+		 x[1][0] = 70; 
+		 x[1][1] = 80;
+		
+		System.out.println(x[0][0]);
+		System.out.println(x[0][1]);
+		System.out.println(x[1][0] );
+		System.out.println(x[1][1]);
+
+	}
+}
